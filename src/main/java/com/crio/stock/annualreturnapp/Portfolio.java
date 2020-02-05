a5f4ea17b6265e9a21e17494c1b5d964d53d485f
Anand Vaidya <anand1711@gmail.com>
2020-02-05 14:23:33 +0530

    integrate with UI

--- src/main/java/com/crio/stock/annualreturnapp/Portfolio.java
+++ src/main/java/com/crio/stock/annualreturnapp/Portfolio.java
@@ -5,10 +5,24 @@ import java.util.List;
 import lombok.Data;
 import lombok.Getter;
 
-@Data
 public class Portfolio {
 
   private String name;
   private List<PortfolioTrade> portfolioTrades;
 
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<PortfolioTrade> getPortfolioTrades() {
+    return portfolioTrades;
+  }
+
+  public void setPortfolioTrades(List<PortfolioTrade> portfolioTrades) {
+    this.portfolioTrades = portfolioTrades;
+  }
 }
