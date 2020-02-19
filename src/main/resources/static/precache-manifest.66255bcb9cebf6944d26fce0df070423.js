e3efc0431e6f752790803c6bcab49d860dfcc32a
Anand Vaidya <anand1711@gmail.com>
2020-02-19 18:36:54 +0530

    integrate UI

---
+++ src/main/resources/static/precache-manifest.66255bcb9cebf6944d26fce0df070423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfb21c82a3542f35bff7679594f2e41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "753c6e4ee003fcc97955",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "44404f0e94c3be15a853",
+    "url": "/static/js/2.3658a489.chunk.js"
+  },
+  {
+    "revision": "0f628f634ecff46d2ac81c95c5b4e1a1",
+    "url": "/static/js/2.3658a489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "753c6e4ee003fcc97955",
+    "url": "/static/js/main.82964a0d.chunk.js"
+  },
+  {
+    "revision": "f530cea0553d2f0a7804",
+    "url": "/static/js/runtime-main.94126ef0.js"
+  }
+]);
