edd08ee7ff9ae327d5e3cf1f1d30821b8ec8ccb4
Anand Vaidya <anand@crio-users.in>
2020-02-28 15:11:29 +0530

    Update AnnualReturnsController.java

--- src/main/java/com/crio/stock/annualreturnapp/AnnualReturnsController.java
+++ src/main/java/com/crio/stock/annualreturnapp/AnnualReturnsController.java
@@ -33,7 +33,7 @@ public class AnnualReturnsController {
   @ResponseBody
   public PortfolioResponse calculateReturns(@RequestBody Portfolio portfolio)
       throws InterruptedException {
-    PortfolioManager portfolioManager = PortfolioManagerFactory.getPortfolioManager("", restTemplate);
+    PortfolioManager portfolioManager = PortfolioManagerFactory.getPortfolioManager(null, restTemplate);
     LocalDate endDate = LocalDate.now().minus(1, ChronoUnit.DAYS);
     try {
       return PortfolioResponse.builder()
